b1dbd3be34af49d3a416ec7d0cecea1bc75f997c
Sonia Soares <sonia.soares@ipvc.pt>
2024-03-09 12:03:09 +0000

    Init Commit

---
+++ app/src/main/java/com/example/lab1_teste/MainActivity2.kt
@@ -0,0 +1,17 @@
+package com.example.lab1_teste
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+
+class MainActivity2 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main2)
+
+        val button: Button = findViewById(R.id.button3)
+        button.setOnClickListener {
+            finish()
+        }
+    }
+}
